ce7a02a7ed695a997e16003a2328c2c8173e964c
Geol Soo Kim <geolsoo.kim@gmail.com>
2017-04-22 23:28:41 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# checkout(url, items)
+
+checkout(url, items) is a function that takes two arguments and returns an integer
+
+The two arguments are:
+
+- url (string): url of json service which returns pricing rule
+- items (list): list of strings representing items and bundles of items
+
+The returned integer is the total price of the list of items
+
+
+## Assumptions
+
+The 'items' argument is a list of strings, each string representing an item or a bundle of n items, where n is the item's special quantity value
+
+The string representing an item is the name of the item in the returned pricing rule
+
+The string representing a bundle of items is the name of the item suffixed with '*'
+
+So for example, if 'https://price_url.com/' is a json service that returns the following pricing rule:
+
+```json
+{
+	"prices": [{
+		"name": "A",
+		"unit_price": 20,
+		"special_qty": 3,
+		"special_price": 50
+	}]
+}
+```
+
+then the string 'AS' represents a bundle of 3 'A' items, and a call to the function
+
+```python
+checkout('https://price_url.com', ['A', 'A*'])
+```
+
+will return the integer 70
---
+++ checkout.py
@@ -0,0 +1,58 @@
+import requests
+
+
+def checkout(url, items):
+    """
+    Args:
+        url (string): url of price service
+        items (list): List of strings, each string representing an item or a bundle of items
+
+    Returns:
+        int: total price of items 
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', ['A'])
+    20
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', ['A*'])
+    50
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', ['A', 'A*'])
+    70
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', ['A', 'A', 'A', 'A'])
+    80
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', ['A', 'A', 'D*'])
+    130
+
+    >>> checkout(' https://api.myjson.com/bins/gx6vz', [])
+    0
+    """
+
+    try:
+        resp = requests.get(url)    
+    except request.exceptions.ConnectionError:
+        return -1
+
+    if resp.status_code != 200:
+        return -1
+
+    try:
+        price_list = resp.json().get('prices')
+    except (ValueError, AttributeError):
+        return -1
+
+    if not price_list:
+        return -1
+
+    unit_prices = {item.get('name') : item.get('unit_price') for item in price_list}
+
+    special_prices = {item.get('name')+'*' : item.get('special_price') for item in price_list if 'special_price' in item}
+
+    all_prices = {**unit_prices, **special_prices}
+
+    total = 0
+    for item in items:
+        total += all_prices.get(item, 0)
+        
+    return total
